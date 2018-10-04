56394ce6180ee395cb6b62955ad668f372bb53b4
Mark <mark.tanner@pagesuite.com>
2018-10-04 12:35:47 +0100

    added unit tests

---
+++ FirePDFTests/PDFTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FirePDF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using System.IO;
+
+namespace FirePDF.Tests
+{
+    [TestClass()]
+    public class PDFTests
+    {
+        private string getPDFFolder()
+        {
+            return Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + "/../../pdfs/";
+        }
+
+        [TestMethod()]
+        public void PDFTest()
+        {
+            string file = getPDFFolder() + "pb13332-cop-cats-091204.pdf";
+            PDF pdf = new PDF(file);
+        }
+    }
+}
