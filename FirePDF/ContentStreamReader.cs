a003a918fae78b95ea1260a3e5dc79d75a468ab2
Mark <mark.tanner@pagesuite.com>
2018-10-05 11:58:55 +0100

    more work on reading content streams

---
+++ FirePDF/ContentStreamReader.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirePDF
+{
+    public static class ContentStreamReader
+    {
+        public static void readContentStream(Stream decompressedStream)
+        {
+            StreamReader reader = new StreamReader(decompressedStream);
+            string commands = reader.ReadToEnd();
+            
+        }
+    }
+}
---
+++ FirePDFTests/ContentStreamReaderTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FirePDF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Reflection;
+
+namespace FirePDF.Tests
+{
+    [TestClass()]
+    public class ContentStreamReaderTests
+    {
+        private string getPDFFolder()
+        {
+            return Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + "/../../pdfs/";
+        }
+
+        [TestMethod()]
+        public void readContentStreamTest()
+        {
+            string file = getPDFFolder() + "pb13332-cop-cats-091204.pdf";
+            PDF pdf = new PDF(file);
+
+            Page page = pdf.getPage(1);
+            Stream s = page.getContentStream();
+            ContentStreamReader.readContentStream(s);
+        }
+    }
+}
