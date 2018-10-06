d45fa6450f59a1ade8f135580028ad449d5f67df
Mark <mark.tanner@pagesuite.com>
2018-10-06 12:26:45 +0100

    starting work for graphics state

---
+++ FirePDF/GraphicsStateProcessor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirePDF
+{
+    class GraphicsStateProcessor
+    {
+        public void processOperation(Operation operation)
+        {
+
+        }
+    }
+}
